6b44f1b2b28443b011d894825976db0c5c71b079
JamesTown-625 <{JamesTown-625}@users.noreply.github.com>
2017-12-23 02:34:31 -0700

    Finished up the last bits of comparison function and got the API to post!

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,20 @@
 // friend array to be compared to as well as added to with new friends. 
-var friendsArray = [
+var friends = [
   {
     name: "Darth Vader",
-    photo: "https://brightcove04pmdo-a.akamaihd.net/3653334524001/3653334524001_5381043941001_5379748916001-vs.jpg?pubId=3653334524001&videoId=5379748916001",
-    scores: [5, 4, 2, 3, 4, 1, 5, 2, 4, 3]
+    photo: "https://needabiggerblog.files.wordpress.com/2014/12/darth-vader_smaller.jpg",
+    scores: ["5", "4", "1", "5", "1", "5", "1", "1", "5", "1"]
   }, {
     name: "Superman",
-    photo: "http://cdn1us.denofgeek.com/sites/denofgeekus/files/styles/main_wide/public/2017/11/justice-league-superman-henry-cavill.jpg?itok=tzWczZXV",
-    scores: [1, 2, 5, 1, 2, 5, 1, 4, 2, 5]
+    photo: "https://d1nao0k9edgivc.cloudfront.net/wp-content/uploads/2013/04/Man-Of-Steel-new-picture.jpg",
+    scores: ["1", "5", "3", "2", "5", "3", "3", "5", "1", "3"]
   }, {
     name: "Gandalf the Grey",
-    photo: "https://static.twproject.com/site/wp-content/uploads/gandalf-01-1024x640-thumb-1024x430.png",
-    scores: [1, 2, 3, 4, 5, 4, 3, 2, 1, 5]
+    photo: "https://hobbydb-production.s3.amazonaws.com/processed_uploads/subject_photo/subject_photo/image/29853/1503507597-28659-9182/GAN.jpg",
+    scores: ["3", "2", "5", "1", "5", "1", "5", "3", "3", "5"]
   }
 ];
 
-module.exports = friendsArray;
+module.exports = friends;
 
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,25 +1,54 @@
-var friendsArray = require('../data/friends.js');
+var friends = require('../data/friends.js');
 
 module.exports = function(app){
   // create a friends API forusers to see all friends that are possible to be matched with - provides JSON
   app.get("/api/friends", function(req, res) {
     
-    return res.json(friendsArray);
-    console.log(friendsArray);
-    console.log("above is the friendsArray");
+    res.json(friends);
+    // console.log(friends);
+    // console.log("above is the friends array");
   });
 
   // Create New friend to be able to get matches - takes in form input
   app.post("/api/friends", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body-parser middleware
-    var newFriend = req.body;
-    console.log(newFriend);
-    console.log("above is the newfriend raw");
-   
-    res.json(newFriend);
-    console.log(newFriend.scores);
-    console.log("above is the new friend array after the json method");
+
+    var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: 1000
+    };
+
+    console.log(req.body);
+
+    //Here we take the result of the user's survey POST and parse it.
+    var userData = req.body;
+    var userScores = userData.scores;
+
+    console.log(userScores);
+
+    var totalDifference = 0;
+
+    for (var i = 0; i < friends.length; i++) {
+      console.log(friends[i]);
+      totalDifference = 0;
+
+      for (var j = 0; j < friends[i].scores[j]; j++) {
+        
+        totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+        if (totalDifference <= bestMatch.friendDifference) {
+
+          bestMatch.name = friends[i].name;
+          bestMatch.photo = friends[i].photo;
+          bestMatch.friendDifference = totalDifference;
+        }
+      }
+    }
+
+    friends.push(userData);
+
+    res.json(bestMatch);
+
   });
 }
  
